0b78d6cfa47a5f4d0c3ede0fb788b9e2c3c6d2c5
cb_2018 <cb_2018@cb-2018deMacBook-Pro.local>
2019-04-16 16:26:26 +0800

    first commit

---
+++ MKJMWWM/Model/MKJDemoModel.h
@@ -0,0 +1,16 @@
+//
+//  MKJDemoModel.h
+//  MKJMWWM
+//
+//  Created by mintou on 2017/3/1.
+//  Copyright © 2017年 mintou. All rights reserved.
+//
+
+#import "MKJBaseModel.h"
+#import <ReactiveCocoa.h>
+
+@interface MKJDemoModel : MKJBaseModel
+
+- (RACSignal *)requestDemoDatasWithPage:(NSInteger)pageNum maxTime:(NSString *)maxTime;
+- (RACSignal *)requestDemoDatasPara:(NSDictionary *)dict entityClass:(Class)clas;
+@end
